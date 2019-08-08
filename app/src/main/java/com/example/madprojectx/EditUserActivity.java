7b2dd4e5cd9c9ab0a2bde9920cc55fcceb560ab4
nishith97 <nishith.pinnawala@gmail.com>
2019-08-08 20:05:31 +0530

    Added string values.

--- app/src/main/java/com/example/madprojectx/EditUserActivity.java
+++ app/src/main/java/com/example/madprojectx/EditUserActivity.java
@@ -11,5 +11,5 @@ public class EditUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+}
 }
